d4b67133bb08264255b52461de2e4b7f30e90b11
oscarjason896@gmail.com <oscarjason896@gmail.com>
2020-08-20 15:19:20 -0600

    Archivos de Proyecto

---
+++ tmp/visual/footer.php
@@ -0,0 +1,19 @@
+<P align="center"><BR></P> 
+<TABLE align="center" width="95%" border="1" cellspacing="1" cellpadding="1" style="color:#fff;">
+  <TBODY>
+  <TR>
+    <TD>&nbsp;Municipalidad de Puerto Cortés &copy; 2020</TD>
+    <TD>
+      <DIV align="center"><a href="https://www.ampuertocortes.hn" target="_blank">www.ampuertocortes.hn</a></DIV></TD>
+    <TD>
+      <DIV align="right">Desarrollo e Innovación               
+      Tecnológica versión 1.0&nbsp;</DIV>
+    </TD>
+  </TR>
+  <TR>
+    <TD colspan="3" align="center">
+<strong>ODS-9 metas 9.4, 9.5, 9.6, 9.7 y 9.8</strong>; POA 2019 incisos 4, 5, 8, 9, 10 y 11; CD resultados 1 y 2
+    </TD>
+  </TR>
+  </TBODY>
+</TABLE>
---
+++ tmp/visual/header.php
@@ -0,0 +1,18 @@
+<P>
+        <IMG align="left" src="images/logo1.png" width="8%" height="8%" border="0">
+        <IMG align="right" src="images/logo1.png" width="8%" height="8%" border="0">
+        
+</P>
+
+<?php
+
+
+
+echo "<br />";
+echo "<div align='center'><b><font face='segoe ui' size=\"7\" color='white'>MUNICIPALIDAD DE PUERTO CORTÉS</color></font></b></div>";
+echo "<br />";
+echo "<div align='center'><b><font face='segoe ui' size=\"5\" color='white'>CONTROL DE DOCUMENTACIÓN MUNICIPAL (CDM)</font></b></div>";
+echo "<br />";
+echo "<br />";
+
+?>
